839b4b56a1881bb09b85bba70226c64039cdebbf
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-07-10 22:35:48 +0600

    Добавьте файлы проекта.

---
+++ Soft.lib/Modul/Shareware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft.lib.Modul
+{
+    public class Shareware : PO
+    {
+        public Shareware(string name, string company, string date, byte demo_period, int cost)
+            : base(name, company, date, demo_period, cost)
+        { }
+
+        public override void Info()
+        {
+            Console.WriteLine("Имя продукта: " + name + "\nПроизводитель: "
+                                 + company + "\nДата установки ПО: " + dateOfInstall.ToShortDateString()
+                                 + "\nПериод бесплатного использования: " + demo_preiod
+                                 + "\nCтоимость продукта: " + cost + " $\n");
+        }
+        public override bool ItIsAWorks()
+        {
+            if (dateOfInstall.AddDays(demo_preiod) >= DateTime.Now)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ SoftWare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soft.lib.Modul;
+
+namespace SoftWare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            PO[] soft = new PO[]{new Free ("GitHub", "GitHub"),
+                            new Shareware("Excel", "microsoft", "04.04.2018", (byte)rnd.Next(), rnd.Next(10,100)),
+                            new Shareware("Notepad", "net", "30.07.2018", (byte)rnd.Next(), rnd.Next(10,100)),
+                            new Commercial("Diablo 3", "BLIZZARD", "05.08.2018", (byte)rnd.Next(100), rnd.Next(100,200), rnd.Next(100)),
+                            new Free("Paint", "Microsoft")};
+
+            Console.WriteLine("~~~~ Весь список ПО:");
+            foreach (PO p in soft)
+                p.Info();
+
+            Console.WriteLine("~~~~ Список ПО, которое можно использовать на сегодняшний день: ");
+            foreach (PO p in soft)
+                if (p.ItIsAWorks()) Console.WriteLine(p.ProgramName);
+
+        }
+    }
+}
